49432f1aaac8f70e4ad593979c543d1e49b4594e
Santidnp <sngh966.sg@gmail.com>
2021-08-07 17:31:34 -0500

    Corrección de main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from basicColor import *
 
 path_user = input("Ingrese el path de la imagen: ")
+h_value = int(input('ingrese un valor de Hue: '))
 
 imagen_user = basicColor(path_user)
 print(imagen_user.displayProperties())
 imagen_user.makeBW()
 
-h_value = int(input('ingrese un valor de Hue: '))
+
 imagen_user.colorize(hue=h_value)
